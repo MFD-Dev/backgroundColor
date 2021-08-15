ff6393f044402c7c29921107d49cd7ba944bee0a
MFD-Dev <65796988+MFD-Dev@users.noreply.github.com>
2021-08-15 17:48:18 -0500

    corrected code with one function

--- app.js
+++ app.js
@@ -4,29 +4,26 @@
 -- variable for color change on each button
 -- create function to change color on click
 */
-// create a variable to grab the button id
-let btnRed = document.querySelector('#btn-red');
-let btnGreen = document.querySelector('#btn-green');
-let btnBlue = document.querySelector('#btn-blue');
-// add a eventListener and a function to hold the code.
-btnRed.addEventListener('click', ()=> {
-    // console.log('here');
-    // this grabbing the entire window and setting it to 
-    // the specific color
-    document.body.style.background  = 'red';
-})
 
-btnGreen.addEventListener('click', ()=> {
-    // console.log('here');
-    // this grabbing the entire window and setting it to 
-    // the specific color
-    document.body.style.background  = 'green';
-})
+//  I want to challenge you to clean this code up a bit by not repeating the same/similar code multiple times.
+// ​
+// Google: javascript get value from select
 
-btnBlue.addEventListener('click', ()=> {
-    // console.log('here');
-    // this grabbing the entire window and setting it to 
-    // the specific color
-    document.body.style.background  = 'blue';
-})
+// Update your select dropdown options to be colors (ex: red, blue, etc.), and write JavaScript code that 
+//sets the background color when the select dropdown is chosen/changed.
 
+// Let me know if you need help with this. You should end up with only one function in your code.
+///////////////////////////////////////////////////////////////////
+document.getElementById('submit').onclick = function() {
+    var checked = document.querySelectorAll('#color-names :checked');
+    var selected = [...checked].map(option => option.value);
+  if (selected == 'btn-red') {
+    document.body.style.backgroundColor = 'red';
+  }
+    else  if (selected == 'btn-green') {
+    document.body.style.backgroundColor = 'green';
+  }
+  else   {
+    document.body.style.backgroundColor = 'blue';
+  }
+}

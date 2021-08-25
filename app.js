be9cddffc09c2ffbac9ec601e5a852b04475c6f2
MFD-Dev <65796988+MFD-Dev@users.noreply.github.com>
2021-08-24 19:00:42 -0500

    finished

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
 
-// document.getElementById('submit').onclick = function() {
-//     var checked = document.querySelectorAll('#color-names :checked');
-//     var selected = [...checked].map(option => option.value);
-//   if (selected == 'red') {
-//     document.body.style.backgroundColor = 'red';
-//   }
-//     else  if (selected == 'green') {
-//     document.body.style.backgroundColor = 'green';
-//   }
-//   else   {
-//     document.body.style.backgroundColor = 'blue';
-//   }
-// }
 
-function handleOnChangeEvent(x) {
-  document.backgroundColor;
+function changeColor() {
+  let color = document.getElementById('color-choice').value;
+  document.body.style.backgroundColor = color;
 }
 
 
-
-// document.getElementById('submit').addEventListener("change", myFunction); 
-
-
-// function myFunction() {
-//     var checked = document.querySelectorAll('#color-names :checked');
-//     var selected = [...checked].map(option => option.value);
-//   if (selected == 'red') {
-//     document.body.style.backgroundColor = 'red';
-//   }
-//     else  if (selected == 'green') {
-//     document.body.style.backgroundColor = 'green';
-//   }
-//   else   {
-//     document.body.style.backgroundColor = 'blue';
-//   }
-// }
